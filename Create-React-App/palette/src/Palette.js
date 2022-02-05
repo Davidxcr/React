de849aea43e83940f341a704be5639241afb2a8e
Davidxcr <vodolatunji@gmail.com>
2022-02-04 19:02:57 -0600

    created react app, added palette and seed file

---
+++ Create-React-App/palette/src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                {/* Navbar goes Here */}
+                <div className='Palette-colors'>
+                    {/* Color boxes to display */}
+                </div>
+                {/* footer */}
+            </div>
+        );
+    }
+}
+
+export default Palette;
